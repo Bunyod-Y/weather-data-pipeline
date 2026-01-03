e469b5d995549208ed6263e9afb73a19eaa12b94
BUNYOD YOKUBOV <117659225+Bunyod-Y@users.noreply.github.com>
2026-01-03 12:44:25 +0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# We use os.getenv so we can read from Docker's environment variables
-API_KEY = os.getenv("API_KEY", "11fd0077382cfac1e7a6d05d8c4aab11") 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "weather_db")
-DB_USER = os.getenv("DB_USER", "myuser")
-DB_PASS = os.getenv("DB_PASSWORD", "mypassword")
